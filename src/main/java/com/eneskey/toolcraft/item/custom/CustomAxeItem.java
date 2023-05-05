b1d4523e61fef0bd1ea67419c99c3ac938bab0bc
kendyiand <hellmax2206@gmail.com>
2023-05-06 02:49:36 +0300

    upgrade version

--- src/main/java/com/eneskey/toolcraft/item/custom/CustomAxeItem.java
+++ src/main/java/com/eneskey/toolcraft/item/custom/CustomAxeItem.java
@@ -1,13 +1,15 @@
 package com.eneskey.toolcraft.item.custom;
 
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -16,7 +18,6 @@ public class CustomAxeItem extends AxeItem {
     public CustomAxeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
     }
-
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
